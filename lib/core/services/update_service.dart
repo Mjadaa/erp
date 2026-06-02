c004f1aaaaceb0fd0459df19d8c25df50e659842
mo <jadaa@gmail.com>
2026-06-02 22:59:52 +0300

    Add appcast for v1.1.1

--- lib/core/services/update_service.dart
+++ lib/core/services/update_service.dart
@@ -2,11 +2,11 @@ import 'package:auto_updater/auto_updater.dart';
 
 // ← غيّر هذا الرابط لرابط الـ appcast.xml على GitHub الخاص بك
 const _feedUrl =
-    'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/appcast.xml';
+    'https://raw.githubusercontent.com/Mjadaa/erp/main/appcast.xml';
 
 Future<void> checkForUpdates() async {
   try {
     await autoUpdater.setFeedURL(_feedUrl);
-    await autoUpdater.checkForUpdatesInBackground();
+    await autoUpdater.checkForUpdates();
   } catch (_) {}
 }
